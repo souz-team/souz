5972d830eb963be730ba4ba4c84bcc6cee2b65ac
Sergey <skirko_sn@mail.ru>
2017-11-01 22:59:00 +0300

    изменил section.php

--- section.php
+++ section.php
@@ -21,29 +21,44 @@ if (isset($_GET['id']))
 				<p class="new-material__title"><?php echo $zagolovok?></p>
 					
 				<div class="new-material__form">
-					<form action="" class="form-new-material">
+					<form action="" class="table-input-info">
 						
-						<label class="form-new-material__row">
-							<p class="form-new-material__label">Название</p>
-							<input type="text" class="form-new-material__textfield">
-						</label>
+						<div class="table-input-info__row">
+							<label class="table-input-info__label">
 
+								<div class="table-input-info__wrap-text">
+									<span class="table-input-info__text">Название</span>
+								</div>
 
-						<div class="form-new-material__row form-new-material__row_selects">
-							<div class="form-new-material__wrap-select">
-								<p class="form-new-material__label">Родитель</p>
-								<select class='form-new-material__select'>
-									<option value="">[Корневой]</option>
-									<option value="">Раздел 1</option>
-									<option value="">Раздел 2</option>
-									<option value="">Раздел 3</option>
-									<option value="">Раздел 4</option>
-									<option value="">Раздел 5</option>
-								</select>
-							</div>
+								<div class="table-input-info__wrap-textfield">
+									<input type="text" class="table-input-info__textfield">
+								</div>
+
+							</label>
 						</div>
 
-						<div class="form-new-material__row form-new-material__row_buttons">
+						<div class="table-input-info__row">
+							<label class="table-input-info__label">
+
+								<div class="table-input-info__wrap-text">
+									<span class="table-input-info__text">Родительский раздел</span>
+								</div>
+
+								<div class="table-input-info__wrap-textfield">
+									<select class='table-input-info__select'>
+										<option value="">[Корневой]</option>
+										<option value="">Раздел 1</option>
+										<option value="">Раздел 2</option>
+										<option value="">Раздел 3</option>
+										<option value="">Раздел 4</option>
+										<option value="">Раздел 5</option>
+									</select>
+								</div>
+
+							</label>
+						</div>
+
+						<div class="table-input-info__buttons">
 							<button class="button button_cancel">Отмена</button>
 							<button class="button button_default">Создать</button>
 						</div>
