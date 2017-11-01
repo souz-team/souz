c82fd363e406e80d8fe3dec52eca106941a11893
diman-sazonov <diman_sazonov@mail.ru>
2017-11-01 22:29:55 +0300

    Изменяет название класса для таблицы вводимой информации

--- blocks/lk-personal-information.php
+++ blocks/lk-personal-information.php
@@ -1,66 +1,66 @@
-<form action="#" class="lk-personal-information">
-	<div class="lk-personal-information__row">
-		<label class='lk-personal-information__label'>
-			<div class="lk-personal-information__wrap-text">
-				<span class="lk-personal-information__text">Имя</span>
+<form action="#" class="table-input-info">
+	<div class="table-input-info__row">
+		<label class='table-input-info__label'>
+			<div class="table-input-info__wrap-text">
+				<span class="table-input-info__text">Имя</span>
 			</div>
-			<div class="lk-personal-information__wrap-textfield">
-				<input type="text" class="lk-personal-information__textfield" value='Олег'>
-			</div>
-		</label>
-	</div>
-	<div class="lk-personal-information__row">
-		<label class='lk-personal-information__label'>
-			<div class="lk-personal-information__wrap-text">
-				<span class="lk-personal-information__text">Фамилия</span>
-			</div>
-			<div class="lk-personal-information__wrap-textfield">
-				<input type="text" class="lk-personal-information__textfield" value='Тинькофф'>
+			<div class="table-input-info__wrap-textfield">
+				<input type="text" class="table-input-info__textfield" value='Олег'>
 			</div>
 		</label>
 	</div>
-	<div class="lk-personal-information__row">
-		<label class='lk-personal-information__label'>
-			<div class="lk-personal-information__wrap-text">
-				<span class="lk-personal-information__text">Email</span>
+	<div class="table-input-info__row">
+		<label class='table-input-info__label'>
+			<div class="table-input-info__wrap-text">
+				<span class="table-input-info__text">Фамилия</span>
 			</div>
-			<div class="lk-personal-information__wrap-textfield">
-				<input type="email" class="lk-personal-information__textfield" value='oleg.tin@mail.ru'>
-			</div>
-		</label>
-	</div>
-	<div class="lk-personal-information__row lk-personal-information__row_delimiter"></div>
-	<div class="lk-personal-information__row">
-		<label class='lk-personal-information__label'>
-			<div class="lk-personal-information__wrap-text">
-				<span class="lk-personal-information__text">Старый пароль</span>
-			</div>
-			<div class="lk-personal-information__wrap-textfield">
-				<input type="password" class="lk-personal-information__textfield">
+			<div class="table-input-info__wrap-textfield">
+				<input type="text" class="table-input-info__textfield" value='Тинькофф'>
 			</div>
 		</label>
 	</div>
-	<div class="lk-personal-information__row">
-		<label class='lk-personal-information__label'>
-			<div class="lk-personal-information__wrap-text">
-				<span class="lk-personal-information__text">Новый пароль</span>
+	<div class="table-input-info__row">
+		<label class='table-input-info__label'>
+			<div class="table-input-info__wrap-text">
+				<span class="table-input-info__text">Email</span>
 			</div>
-			<div class="lk-personal-information__wrap-textfield">
-				<input type="password" class="lk-personal-information__textfield">
-			</div>
-		</label>
-	</div>
-	<div class="lk-personal-information__row">
-		<label class='lk-personal-information__label'>
-			<div class="lk-personal-information__wrap-text">
-				<span class="lk-personal-information__text">Повторите новый пароль</span>
-			</div>
-			<div class="lk-personal-information__wrap-textfield">
-				<input type="password" class="lk-personal-information__textfield">
+			<div class="table-input-info__wrap-textfield">
+				<input type="email" class="table-input-info__textfield" value='oleg.tin@mail.ru'>
 			</div>
 		</label>
 	</div>
-	<div class="lk-personal-information__buttons">
+	<div class="table-input-info__row table-input-info__row_delimiter"></div>
+	<div class="table-input-info__row">
+		<label class='table-input-info__label'>
+			<div class="table-input-info__wrap-text">
+				<span class="table-input-info__text">Старый пароль</span>
+			</div>
+			<div class="table-input-info__wrap-textfield">
+				<input type="password" class="table-input-info__textfield">
+			</div>
+		</label>
+	</div>
+	<div class="table-input-info__row">
+		<label class='table-input-info__label'>
+			<div class="table-input-info__wrap-text">
+				<span class="table-input-info__text">Новый пароль</span>
+			</div>
+			<div class="table-input-info__wrap-textfield">
+				<input type="password" class="table-input-info__textfield">
+			</div>
+		</label>
+	</div>
+	<div class="table-input-info__row">
+		<label class='table-input-info__label'>
+			<div class="table-input-info__wrap-text">
+				<span class="table-input-info__text">Повторите новый пароль</span>
+			</div>
+			<div class="table-input-info__wrap-textfield">
+				<input type="password" class="table-input-info__textfield">
+			</div>
+		</label>
+	</div>
+	<div class="table-input-info__buttons">
 		<button class="button button_default">Сохранить</button>
 	</div>
 </form>
