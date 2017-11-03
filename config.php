a990262adcfaa73f801ab295ffe169f30b17d382
Shpanerov <DragoNext52@gmail.com>
2017-11-03 09:07:15 +0300

    Изменён config.php: Если нет подключения на удалённый сервер, то используется локальный

--- config.php
+++ config.php
@@ -1,25 +1,29 @@
 <?php
 session_start();
 
-$host = "5.164.244.129:8888";
-//$host = "localhost";
+$host = "5.166.216.141:8888";
 $db_name = "SOUZ";
 $login = "admin";
 $pswrd = "15istv1";
-$connect = mysql_connect ("$host", "$login", "$pswrd");
-mysql_set_charset("utf8");
-
+$connect = @mysql_connect ("$host", "$login", "$pswrd");		
+//Если подключение не открылось на удалённый сервер, то используем локальный
 if (!$connect) {
-	
-    echo "dont work";
-    exit (mysql_error ()); 
+	$host = "127.0.0.1";
+    $connect = mysql_connect ("$host", "$login", "$pswrd");
+	mysql_set_charset("utf8");
 }
+if(!$connect) {
+	echo "Удалённый сервер баз данных недоступен, а локальный не отвечает";
+	exit (mysql_error ());
+}  
 else {
 	
     mysql_select_db ("$db_name", $connect);
+	mysql_set_charset("utf8");
 	  
 }
-   global $connect; 
+	
+	global $connect; 
 mysql_query ("SET NAMES 'UTF-8'");
 $link = mysqli_connect($host, $login, $pswrd, $db_name) or die("Ошибка " . mysqli_error($link));
 include '/func/db.php';
