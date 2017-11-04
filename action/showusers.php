832ca53862a8f4b01161da4b67677c461138fa08
Sergey <skirko_sn@mail.ru>
2017-11-05 02:43:33 +0300

    управление юзерами

--- action/showusers.php
+++ action/showusers.php
@@ -1,8 +1,8 @@
 <?php
 $sort_id = array ('login','name','surname','email','reg_date','level_id');
 
-$dir = array('login'=>0, 'name'=>0, 'surname'=>0, 'email'=>0, 'reg_date'=>0, 'level_id'=>0);
-$asc = 0;
+$dir = array('login'=>'arrow.png', 'name'=>'arrow.png', 'surname'=>'arrow.png', 'email'=>'arrow.png', 'reg_date'=>'arrow.png', 'level_id'=>'arrow.png');
+
 if(isset($_GET['sort']))
 {
 	$a = $_SESSION['direct'];
@@ -10,11 +10,11 @@ if(isset($_GET['sort']))
 	++$a;
 	if($a%=2){
 		$direct = 'DESC';
-		$asc = 31;
+		$asc = '22.png';
 	}
 	else{
 		$direct = 'ASC';
-		$asc = 30;
+		$asc = '21.png';
 	}
 
 	$_SESSION['direct'] = $a;
@@ -22,6 +22,7 @@ if(isset($_GET['sort']))
 	$get_sort= filter_input(INPUT_GET, 'sort', FILTER_VALIDATE_INT);
 	$sort_key=$sort_id[$get_sort-1];
 	$dir[$sort_key] = $asc;
+	
 }
 $array = Show_Users ($link, $sort_key, $direct); // возвращение многомерного массива по всем строкам найденного в функции Search_id отдела.
 ?>
--- blocks/cu-personal.php
+++ blocks/cu-personal.php
@@ -7,12 +7,12 @@ require_once '/action/showusers.php';
 	<tr bgcolor="#FFFFFF" align="center">
 
 		<th rowspan="2">№</th>
-		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=1' title = 'Сортировка по логину'><?=chr($dir[login])?> Логин*</a></th>
-		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=2' title = 'Сортировка по имени'><?=chr($dir[name])?> Имя*</a></th>
-		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=3' title = 'Сортировка по фамилии'><?=chr($dir[surname])?> Фамилия*</th>
-		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=4' title = 'Сортировка по email'><?=chr($dir[email])?> Email*</th>
-		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=5' title = 'Сортировка по дате регистрации'><?=chr($dir[reg_date])?> Дата регистрации*</th>
-		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=6' title = 'Сортировка по уровню'><?=chr($dir[level_id])?> Уровень пользователя*</th>
+		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=1' title = 'Сортировка по логину'><img src='/images/<?=$dir[login]?>' alt=''> Логин</a></th>
+		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=2' title = 'Сортировка по имени'><img src='/images/<?=$dir[name]?>'> Имя</a></th>
+		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=3' title = 'Сортировка по фамилии'><img src='/images/<?=$dir[surname]?>'> Фамилия</th>
+		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=4' title = 'Сортировка по email'><img src='/images/<?=$dir[email]?>'> Email</th>
+		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=5' title = 'Сортировка по дате регистрации'><img src='/images/<?=$dir[reg_date]?>'> Дата регистрации</th>
+		<th rowspan="2"><a href='<?= $_SERVER[PHP_SELF]?>?sort=6' title = 'Сортировка по уровню'><img src='/images/<?=$dir[level_id]?>'> Уровень пользователя</th>
 		<th colspan="2">Операции с пользователем</th>
 	
 	</tr>
