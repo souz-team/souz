29fab3099f0730a67b4f73d104dad0a76c685376
Yavorskaya <taffytf@gmail.com>
2017-11-05 20:03:58 +0300

    Изменения в файле forum-razdel.php. Выводится последняя дата в разделе, а не в последней теме

--- blocks/forum-razdel.php
+++ blocks/forum-razdel.php
@@ -21,17 +21,21 @@
 				//Подсчет количества тем в разделе
 					$num_rows_theme = Topic_Amount($link, $section_id);
                     $num_rows_post = 0;
+                    $tempdate = '0000-00-00';
                     for ($j=0; $j<count($theme); $j++) {
-                        $tempdate = '0000-00-00';
+                       
                         $theme_id = $theme[$j]['theme_id'];
 			//Подсчет количества сообщений в разделе
                         $num_rows_post += Post_Amount($link, $theme_id);
 			//Определение даты последнего сообщения
                         $date =  Last_Date($link, $theme_id);
                         $date_post = $date[0]; // Вернуть строку с датой последнего сообщения
-                        $maxdate = max($date_post, $tempdate);
-                        $tempdate = $maxdate;
-                        if ($maxdate == '0000-00-00') $maxdate = NULL;
+                        if ($date)
+                        {
+                            $maxdate = max($date_post, $tempdate);
+                            $tempdate = $maxdate;
+                            if ($maxdate == '0000-00-00') $maxdate = NULL;
+                        }
                     }
 				
 				if ($section[$i]['close']!=1)
