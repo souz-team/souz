26bfe1b46904df0fff7a5ab0e9e2c9412fd6e8eb
AnnSalt <saltykova111@mail.ru>
2017-11-07 14:40:28 +0300

    Изменения в создание статьи

--- create-article.php
+++ create-article.php
@@ -3,11 +3,55 @@
 	
 	$authorName = $_SESSION['fio'];
 	$idPodRazdel = $_POST['id_Podrazdel'];
-	$artName = $_POST['articleName'];
-	$artText = $_POST['articleText'];
-	$strSQL = "INSERT INTO `Articles` (`id_Podrazdel`, `Name`, `Author`, `Text`, `Date`) VALUES( $idPodRazdel, '$artName', '$authorName', '$artText', Now() )";
-
-	mysql_query($strSQL) or die (mysql_error());
+	
+	if (isset($_POST["articleName"]))
+	{
+		$artName = $_POST["articleName"];
+	}
+	if ($artName == "")
+	{
+		$err_str.='Поле "Название" пустое <br>';
+	}
+	if (strlen ($artName) > 200)
+	{
+		$err_str.= 'Поле "Название" слишком длинное <br>';
+	}
+		
+		
+	if (isset($_POST["articleText"]))
+	{
+		$artText = $_POST["articleText"];
+	}
+	if ($artText == "")
+	{
+		$err_str.='Поле "Текст статьи" пустое <br>';
+	}
+	
+	if( isset($_POST["articleName"])
+	 && isset($_POST["articleText"]))
+		{
+		echo $err_str;
+		}
+	
+	function cheсk_post($link, $var)
+			{
+				return mysqli_real_escape_string( $link,  $_POST[$var] );
+			}
+	
+	if( isset($_POST["articleName"])
+	 && isset($_POST["articleText"])
+	 && $err_str == null )
+		{
+			$str1 = cheсk_post($link, 'articleName');
+			$str2 = cheсk_post($link, 'articleText');
+			
+			$artName = htmlentities($str1, ENT_QUOTES);
+			$artText = htmlentities($str2, ENT_QUOTES);
+				
+			$strSQL = "INSERT INTO `Articles` (`id_Podrazdel`, `Name`, `Author`, `Text`, `Date`) VALUES( $idPodRazdel, '$artName', '$authorName', '$artText', Now() )";
+			mysql_query($strSQL) or die (mysql_error());
+	
+		}
 
 	header('Location: manage-articles.php');
 ?>
