d5ee9bb0a065d2efe5ea39e5216646407853ab90
diman-sazonov <diman_sazonov@mail.ru>
2017-11-20 23:51:55 +0300

    Создает popup-окно на удаление статьи.

--- manage-articles.php
+++ manage-articles.php
@@ -32,21 +32,16 @@
 						<?php
 									$array = Show_Articles($link, $podrazdelId);
 									$counter = 0;
-									for  ($i=0; $i<count($array); $i++) {
+									for ($i=0; $i<count($array); $i++) {
 										?>
-											<div class="manage-table__row manage-table__row_body" entity-id='<?=$array[$i]['id'] ?>'> 
-											<div class="manage-table__cell manage-table__cell_name">
-                                        
-											<?echo $array[$i]['Name'] ?></a>
-											</div>
+											<div class="manage-table__row manage-table__row_body" entity-id='<?=$array[$i]['id'] ?>' podrazdel-id='<?= $podrazdelId ?>'>
+											<div class="manage-table__cell manage-table__cell_name"><?= $array[$i]['Name'] ?></div>
 											<div class="manage-table__cell manage-table__cell_author"><?echo $array[$i]['Author'] ?></div>
 											<div class="manage-table__cell manage-table__cell_date"><?echo $array[$i]['Date'] ?></div>
 											<div class="manage-table__cell manage-table__cell_actions">
-											<a href="/article-edit.php?artId=<?=$array[$i]['id']?>&podrazId=<?=$podrazdelId?>" class="manage-table__action-link">Изменить</a>
-											<a href="/article-delete.php?del_id=<?=$array[$i]['id']?>&podrazId=<?=$podrazdelId?>" onclick="return confirm('Подтвердите удаление')? true : false;" class="manage-table__action-link manage-table__action-link_remove" >Удалить</a>
-											
-									
-									</div>
+												<a href="/article-edit.php?artId=<?=$array[$i]['id']?>&podrazId=<?=$podrazdelId?>" class="manage-table__action-link">Изменить</a>
+												<a href="#" class="manage-table__action-link manage-table__action-link_remove">Удалить</a>
+											</div>
 									
 								</div>
 							<?}  ?>
