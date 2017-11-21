269210ba1aa77ae989cddc9bb5ae9996f5d90c09
dmitry-mera <dmitry.sazonov.off@mail.ru>
2017-11-21 14:00:15 +0300

    Добавляет столбец Модератор в таблицу Разделов/Подразделов

--- manage-sections.php
+++ manage-sections.php
@@ -13,42 +13,58 @@ if ($_SESSION['userlevel']==1 || $_SESSION['userlevel']==2){ ?>
 						<div class="manage-table__header">
 							<div class="manage-table__row manage-table__row_header">
 								<div class="manage-table__cell manage-table__cell_header manage-table__cell_header-name manage-table__cell_name">Название</div>
+								<div class="manage-table__cell manage-table__cell_header manage-table__cell_moderator">Модератор</div>
 								<div class="manage-table__cell manage-table__cell_header manage-table__cell_count-articles">Кол-во статей</div>
 								<div class="manage-table__cell manage-table__cell_header manage-table__cell_actions">Действия</div>
 							</div>
 						</div>
 						<div class="manage-table__body">
-							<?php 
-							$razdel = Menu($link, 0); // Передаём нуль для поиска всех корневых разделов
-								for ($i=0; $i<count($razdel); $i++) { 
-									$id = $razdel[$i]['id'];?>
-								<div class="manage-table__row manage-table__row_body" entity-id='<?= $id ?>'>
+							<?php
+								// Передаём нуль для поиска всех корневых разделов
+								$sections = Menu($link, 0);
+
+							?>
+
+							<?php foreach ($sections as $section) { ?>
+
+								<div class="manage-table__row manage-table__row_body" entity-id="<?= $section['id'] ?>">
 									<div class="manage-table__cell manage-table__cell_name manage-table__cell_section-name">
-										<a href='#' class='manage-table__name-link'><?php echo $razdel[$i]['Name']?></a>
+										<a href='#' class='manage-table__name-link'><?= $section['Name'] ?></a>
 									</div>
+
+									<div class="manage-table__cell manage-table__cell_moderator">moderator</div>
 									<div class="manage-table__cell manage-table__cell_count-articles"></div>
 									<div class="manage-table__cell manage-table__cell_actions">
-										<a href="/section-change.php?id=<?= $id ?>" class="manage-table__action-link">Изменить</a>
+										<a href="/section-change.php?id=<?= $section['id'] ?>" class="manage-table__action-link">Изменить</a>
 										<a href="#" class="manage-table__action-link manage-table__action-link_remove">Удалить</a>
-										<a href="/section-create.php?sectionParent=<?= $id ?>" class="manage-table__action-link">Создать подраздел</a>
+										<a href="/section-create.php?sectionParent=<?= $section['id'] ?>" class="manage-table__action-link">Создать подраздел</a>
 									</div>
 								</div>
-								<?php 
-            						$podrazdel = Menu($link, $id); // Передаём id раздела для поиска всех подразделов. P_id == id
-           						    for ($j=0; $j<count($podrazdel); $j++) { 
-           						    	$id = $podrazdel[$j]['id']?>
-									<div class="manage-table__row manage-table__row_subsection manage-table__row_body" entity-id='<?= $id ?>'>
+
+								<?php
+									// Передаём id раздела для поиска всех подразделов. P_id == id
+									$subsections = Menu($link, $section['id']);
+
+								?>
+
+								<?php foreach ($subsections as $subsection) { ?>
+
+									<div class="manage-table__row manage-table__row_subsection manage-table__row_body" entity-id="<?= $subsection['id'] ?>">
 										<div class="manage-table__cell manage-table__cell_name">
-											<a href='manage-articles.php?id=<?= $podrazdel[$j]['id'] ?>' class='manage-table__name-link'> <?echo $podrazdel[$j]['Name']?></a>
+											<a href='manage-articles.php?id=<?= $subsection['id'] ?>' class='manage-table__name-link'><?= $subsection['Name'] ?></a>
 										</div>
-										<div class="manage-table__cell manage-table__cell_count-articles"><?= Articles_Amount($link, $id) ?></div>
+										<div class="manage-table__cell manage-table__cell_moderator">moderator</div>
+										<div class="manage-table__cell manage-table__cell_count-articles"><?= Articles_Amount($link, $subsection['id']) ?></div>
 										<div class="manage-table__cell manage-table__cell_actions">
-											<a href="/section-change.php?id=<?= $id ?>" class="manage-table__action-link">Изменить</a>
+											<a href="/section-change.php?id=<?= $subsection['id'] ?>" class="manage-table__action-link">Изменить</a>
 											<a href="#" class="manage-table__action-link manage-table__action-link_remove">Удалить</a>
 										</div>
 									</div>
+
 								<?php } ?>
+
 							<?php } ?>
+
 						</div>
 					</div>
 				</div>
