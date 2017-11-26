be3e3cbdf901b8fdd3395c9ad5b91af91a9d373a
dmitry-sazonov <diman_sazonov@mail.ru>
2017-11-26 21:01:21 +0300

    Добавляет скролл в таблицу тем, которая в ЛК

--- blocks/lk-topics.php
+++ blocks/lk-topics.php
@@ -13,22 +13,21 @@ require '/action/get-topic-by-user.php';
 			<div class="manage-table__cell manage-table__cell_header manage-table__cell_date">Опубликовано</div>
 		</div>
 	</div>
-	<div class="manage-table__body">
+	<div class="manage-table__body manage-table__body_scroll">
 	<?php if(!empty($topics)){?>
 		<?php foreach($topics as $i => $topic) { ?>
-		<?php $i=$i+1 ?>
-		<div class="manage-table__row manage-table__row_body">
-			<div class="manage-table__cell manage-table__cell_name">
-				<a href='/topic-view.php?id=<?= $topic['theme_id']?>' class='manage-table__name-link'><?=$i.'. '.$topic['subject']?></a>
+			<div class="manage-table__row manage-table__row_body">
+				<div class="manage-table__cell manage-table__cell_name">
+					<a href='/topic-view.php?id=<?= $topic['theme_id']?>' class='manage-table__name-link'><?=($i+1).'. '.$topic['subject']?></a>
+				</div>
+				<?php
+					$date =  mysqli_query ($link, "SELECT MAX(m_date) FROM boardp WHERE theme_id=$topic[theme_id]");
+					$row_date = mysqli_fetch_array($date);
+					$date_post = $row_date[0];
+				?>
+				<p class="manage-table__cell manage-table__cell_date"><?=$date_post?></p>
+				<p class="manage-table__cell manage-table__cell_date"><?=$topic['create_date']?></p>
 			</div>
-			<?php
-			$date =  mysqli_query ($link, "SELECT MAX(m_date) FROM boardp WHERE theme_id=$topic[theme_id]");
-			$row_date = mysqli_fetch_array($date);
-			$date_post = $row_date[0];
-			?>
-			<p class="manage-table__cell manage-table__cell_date"><?=$date_post?></p>
-			<p class="manage-table__cell manage-table__cell_date"><?=$topic['create_date']?></p>
-		</div>
 		<?php } ?>
 	<?php } else { ?>
 	<p> Вы не создали ни одной темы на форуме </p>
