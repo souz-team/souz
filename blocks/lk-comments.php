5d66ea156885a427dae90840c625588c3021b48d
Sergey <skirko_sn@mail.ru>
2017-11-28 00:35:21 +0300

    мелочь

--- blocks/lk-comments.php
+++ blocks/lk-comments.php
@@ -1,19 +1,31 @@
 <?php
 require '/action/get_user_info.php';
-require '/action/get-section-by-user-id.php';
+require '/action/get-article-by-user.php';
 ?>
-<div class="lk-comments">
-  <?php //for($i=1; $i<5; $i++) { ?>
-  <?php if($section_id) { ?>
-  <?php foreach ($section_id as $section) { ?>
-  <?php $section_name = Show_Podrazdel($link, $section['section_id']) ?>
-	 <div class="lk-comments__item"> 
-    <a href='#' class="lk-comments__topic-name"><?=$section_name['Name']?></a>
-   <!-- <p class="lk-comments__comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde earum placeat mollitia maiores ratione, accusantium corrupti eos. Impedit, consectetur doloremque!</p>
-    <p class="lk-comments__date">19.10.2010</p> -->
-  </div>
-  <?php } ?>
-  <?php } else {?>
-  <p>Мне не доверили ни одного раздела</p>
-  <?php } ?>
+<div class="lk-topics manage-table">
+
+	<div class="manage-table__header">
+	
+		<div class="manage-table__row manage-table__row_header">
+		
+			<div class="manage-table__cell manage-table__cell_header manage-table__cell_header-name manage-table__cell_name">Название</div>
+			
+			<div class="manage-table__cell manage-table__cell_header manage-table__cell_date">Опубликовано</div>
+		</div>
+	</div>
+	<div class="manage-table__body manage-table__body_scroll">
+	<?php if(!empty($topics)){?>
+		<?php foreach($topics as $i => $topic) { ?>
+			<div class="manage-table__row manage-table__row_body">
+				<div class="manage-table__cell manage-table__cell_name">
+					<a href='/topic-view.php?id=<?= $topic['theme_id']?>' class='manage-table__name-link'><?=($i+1).'. '.$topic['subject']?></a>
+				</div>
+				
+				<p class="manage-table__cell manage-table__cell_date"><?=$topic['create_date']?></p>
+			</div>
+		<?php } ?>
+	<?php } else { ?>
+	<p> Вы не создали ни одной темы на форуме </p>
+		<?php } ?>
+	</div>
 </div>
