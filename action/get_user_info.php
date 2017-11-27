efc165808e895e1ad55d2ba63d44e130229b70b6
Sergey <skirko_sn@mail.ru>
2017-11-27 19:24:52 +0300

    Добавил отображение в ЛК администрируемых разделов в статьях

--- action/get_user_info.php
+++ action/get_user_info.php
@@ -2,7 +2,7 @@
 	$auth_login = $_SESSION['login'];
 	
 	$reg_info = Login_Exist($link, $auth_login);
-	
+	$user_id = $reg_info['id'];
 	$name = $reg_info['name'];
 	$surname = $reg_info['surname'];
 	$email = $reg_info['email'];
